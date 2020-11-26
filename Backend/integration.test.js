ac9019d4e459d098bd8730ed58dc0f0ce657e6b7
Kieran Weaver <drawbook.productions@gmail.com>
2020-11-25 17:16:42 -0800

    Add addEventToUser test case and improve integration test

--- Backend/integration.test.js
+++ Backend/integration.test.js
@@ -14,14 +14,50 @@ const app = require("./app.js");
 const db = require("./database.js");
 const uri = "mongodb://127.0.0.1:27017/";
 const dbname = "IntegrationTest";
+const start1 = new Date(2020, 10, 24, 10, 45);
+const end1 = new Date(2020, 10, 24, 13, 50);
+const start2 = new Date(2020, 10, 25, 10, 45);
+const end2 = new Date(2020, 10, 25, 13, 50);
+const ev1 = {
+    name: "TestEventName1",
+    description: "TestDesc1",
+    start: start1,
+    end: end1,
+    location: {"lat":0, "long":0}
+};
+const ev2 = {
+    name: "TestEventName2",
+    description: "TestDesc2",
+    start: start2,
+    end: end2,
+    location: {"lat":0, "long":0}
+};
+const ev3 = {
+    name: "TestEventName3",
+    description: "TestDesc3",
+    start: start1,
+    end: end2,
+    location: {"lat":0, "long":0}
+};
 
-test("Testing Login", async () => {
-    await db.close();
-    await db.init(uri, dbname);
-    await db.clear();
+beforeAll(() => {
+    return db.init(uri, dbname);
+});
+
+beforeEach(() => {
+    return db.clear();
+});
+
+afterAll(() => {
+    return db.close();
+});
+
+test("Testing Login", async () => {    
+    // Test Case 1: Add user, should work
     const post1 = await request(app).post("/user/TestUser").send({});
     expect(post1.statusCode).toBe(201);
 
+    // Test Case 2: Adding the same user should detect a conflict
     const post2 = await request(app).post("/user/TestUser").send({});
     expect(post2.statusCode).toBe(409);
 
@@ -31,29 +67,9 @@ test("Testing Login", async () => {
         id : "TestUser",
         events: []
     });
-    
-    await db.close();
 });
 
 test("Testing Create Event", async () => {
-    const start1 = new Date(2020, 10, 24, 10, 45);
-    const end1 = new Date(2020, 10, 24, 13, 50);
-    const start2 = new Date(2020, 10, 25, 10, 45);
-    const end2 = new Date(2020, 10, 25, 13, 50);
-    const ev1 = {
-        name: "TestEventName1",
-        description: "TestDesc1",
-        start: start1,
-        end: end1,
-        location: {"lat":0, "long":0}
-    };
-    const ev2 = {
-        name: "TestEventName2",
-        description: "TestDesc2",
-        start: start2,
-        end: end2,
-        location: {"lat":0, "long":0}
-    };
     const expected = [
     {
         attendees: [],
@@ -71,11 +87,6 @@ test("Testing Create Event", async () => {
         end: end2.toISOString(),
         location: {"lat":0, "long":0}
     }];
-
-    
-    await db.close();
-    await db.init(uri, dbname);
-    await db.clear();
     
     const addResponse1 = await request(app).post("/event/").send({});
     expect(addResponse1.statusCode).toBe(201);
@@ -93,24 +104,9 @@ test("Testing Create Event", async () => {
         length : 2,
         events : expected
     });
-    await db.close();
 });
 
 test("Testing Delete Event", async () => {
-    const start1 = new Date(2020, 10, 24, 10, 45);
-    const end1 = new Date(2020, 10, 24, 13, 50);
-    const ev1 = {
-        name: "TestEventName1",
-        description: "TestDesc1",
-        start: start1,
-        end: end1,
-        location: {"lat":0, "long":0}
-    };
-
-    await db.close();
-    await db.init(uri, dbname);
-    await db.clear();
-
     const addResponse = await request(app).post("/event/").send(ev1);
     expect(addResponse.statusCode).toBe(201);
 
@@ -119,12 +115,44 @@ test("Testing Delete Event", async () => {
 
     const delResponse2 = await request(app).delete(`/event/${addResponse.body.id}`);
     expect(delResponse2.statusCode).toBe(404);
-
-    await db.close();
 });
 
 test("Testing Add Event To User", async () => {
-    assert(false);
+    // Preparation: Add 3 events and a test user
+    const EV1Res = await request(app).post("/event/").send(ev1);
+    expect(EV1Res.statusCode).toBe(201);
+    
+    const EV2Res = await request(app).post("/event/").send(ev2);
+    expect(EV2Res.statusCode).toBe(201);
+    
+    const EV3Res = await request(app).post("/event/").send(ev3);
+    expect(EV3Res.statusCode).toBe(201);
+
+    const post1 = await request(app).post("/user/TestUser").send({});
+    expect(post1.statusCode).toBe(201);
+
+    // Test Case 1: Adding an event that does not exist should return 404
+    const post2 = await request(app).post("/user/TestUser/event/-1").send({});
+    expect(post2.statusCode).toBe(404);
+    
+    // Test Case 2: Adding a valid event to an empty user should work
+    const post3 = await request(app).post(`/user/TestUser/event/${EV1Res.body.id}`);
+    expect(post3.statusCode).toBe(200);
+    
+    // Test Case 3: ev3 and ev1 conflict, which should be indicated in the response
+    const post4 = await request(app).post(`/user/TestUser/event/${EV3Res.body.id}`);
+    expect(post4.statusCode).toBe(409);
+    
+    // Test Case 4: ev1 and ev2 do not conflict, so ev2 should be added
+    const post5 = await request(app).post(`/user/TestUser/event/${EV2Res.body.id}`);
+    expect(post5.statusCode).toBe(200);
+
+    const get1 = await request(app).get("/user/TestUser");
+    expect(get1.statusCode).toBe(200);
+    expect(get1.body).toEqual({
+        id : "TestUser",
+        events: [EV1Res.body.id, EV2Res.body.id]
+    });
 });
 
 test("Testing Remove Event From User", async () => {
